ef3cceaf652f273d9a7065cbd41c0055a026c1e0
Oleksandr <sashapylypenko.pov@gmail.com>
2026-02-22 11:48:22 +0200

    fix: eslint errors

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -6,15 +6,15 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
   },
-  extends: ['eslint:recommended', 'google'],
+  extends: ["eslint:recommended", "google"],
   rules: {
-    'no-restricted-globals': ['error', 'name', 'length'],
-    'prefer-arrow-callback': 'error',
-    quotes: ['error', 'double', { allowTemplateLiterals: true }],
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
   },
   overrides: [
     {
-      files: ['**/*.spec.*'],
+      files: ["**/*.spec.*"],
       env: {
         mocha: true,
       },
